d3239a650b7742485c291bd12f09037b418d3fc0
luis <lfflorian4@gmail.com>
2018-10-13 18:58:49 -0600

    Configuraciones iniciales

---
+++ index.js
@@ -0,0 +1,15 @@
+'use strict'
+
+const mongoose = require('mongoose')
+const app = require('./app')
+const config = require('./config')
+
+mongoose.connect(config.db, (err, res) => {
+    if (err) {
+        console.log('Hubo un error en la conexión a la base de datos: ' + err)
+    }
+})
+
+app.listen(config.port, () => {
+    console.log('Aplicación en puerto: ' + config.port)
+})
