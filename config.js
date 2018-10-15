111bcf704269e0590f46f56b2af12f712633e959
luis <lfflorian4@gmail.com>
2018-10-14 22:38:25 -0600

    Cambio de puerto

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    port: process.env.PORT || 3000,
+    port: process.env.PORT || 3030,
     db: process.env.MONGODB || 'mongodb://localhost:27017/Mail',
     mailUser: 'lfflorian4@gmail.com',
     mailPass: 'florian$93lffr'
