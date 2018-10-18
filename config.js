fe2a7aa424ff56765bf1a858090c9afd305b4480
Luis Fernando Florian <lflorian@mega.corp>
2018-10-18 11:15:50 -0600

    cambio de autenticacion db

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     port: process.env.PORT || 3030,
-    db: process.env.MONGODB || 'mongodb://lfflorian:admin123*@ds147821.mlab.com:47821/mail',
+    db: process.env.MONGODB || 'mongodb://lfflorian:admin123@ds147821.mlab.com:47821/mail',
     mailUser: 'lfflorian4@gmail.com',
     mailPass: 'florian$93lffr'
 }
