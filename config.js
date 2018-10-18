24b66193ef4a09baf1d1edf0abb26e7bfe3c9f1f
Luis Fernando Florian <lflorian@mega.corp>
2018-10-18 11:00:22 -0600

    Cambio de base de datos

--- config.js
+++ config.js
@@ -1,6 +1,8 @@
 module.exports = {
     port: process.env.PORT || 3030,
-    db: process.env.MONGODB || 'mongodb://localhost:27017/Mail',
+    db: process.env.MONGODB || 'mongodb://lfflorian:admin123*@ds147821.mlab.com:47821/mail',
     mailUser: 'lfflorian4@gmail.com',
     mailPass: 'florian$93lffr'
 }
+
+//mongodb://localhost:27017/Mail
